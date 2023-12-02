b6b8fc99a4cc69084437c357b1f450c19d2a0bb8
zxcnevermore <lilskies1@mail.ru>
2023-12-03 01:30:54 +0200

    cloud-storage-back

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateUserDto {
+  @ApiProperty()
+  email: string;
+  @ApiProperty()
+  fullName: string;
+  @ApiProperty()
+  password: string;
+
+}
